aba2b9e520bc0db34c15a55099aa76e495cee960
Mikołaj Małkiński <m.malkinski87@gmail.com>
2019-11-13 14:54:15 +0100

    Fetching person details from Messenger API

--- sharesio/repository.py
+++ sharesio/repository.py
@@ -1,14 +1,33 @@
 class ImageInMemoryRepository:
     def __init__(self):
-        self._map = {}
+        self._images = {}
 
     def save(self, user_id, image_url):
-        if user_id not in self._map.keys():
-            self._map[user_id] = set()
-        self._map[user_id].add(image_url)
+        if user_id not in self._images.keys():
+            self._images[user_id] = set()
+        self._images[user_id].add(image_url)
 
-    def get_images_by_user_id(self, user_id):
-        if user_id in self._map.keys():
-            return self._map[user_id]
+    def find_all_by_user_id(self, user_id):
+        if user_id in self._images.keys():
+            return self._images[user_id]
         else:
             return []
+
+
+class UserInMemoryRepository:
+    def __init__(self):
+        self._users = []
+
+    def save(self, user):
+        if not self.find_by_page_scoped_id(user.page_scoped_id):
+            self._users.append(user)
+
+    def find_by_page_scoped_id(self, id):
+        return filter(lambda u: u.page_scoped_id == id, self._users)
+
+
+class User:
+    def __init__(self, page_scoped_id, first_name, last_name):
+        self.page_scoped_id = page_scoped_id
+        self.first_name = first_name
+        self.last_name = last_name
